03bee9425a8147ea9d258a54e6b7c1d16f46a428
javier526 <twitch7894@gmail.com>
2018-09-21 17:38:25 -0300

    add Counter

--- src/Counter.js
+++ src/Counter.js
@@ -1,13 +1,35 @@
 import React, { Component } from 'react';
 
 export default class Counter extends Component {
+  state = {
+    Count: 1
+  }
+  handleClickIncrement = () => {
+      this.setState({
+        Count: this.state.Count + 1
+      })
+  } 
+ handleClickDecrement = () => {
+   if (this.state.Count >= 1) {
+     this.setState({
+         Count: this.state.Count - 1,
+     })
+   }
+ }
+ handleClickReset = () => {
+   this.setState({
+     Count: 0,
+   })
+ }
   render() {
+    const { Count } = this.state;
+    console.log(Count);
     return (
       <section className="Counter">
-        <h1>Count: {0}</h1>
-        <button onClick={() => {}} className="full-width">Increment</button>
-        <button onClick={() => {}} className="full-width">Decrement</button>
-        <button onClick={() => {}} className="full-width">Reset</button>
+        <h2>Count: {Count}</h2>
+        <button onClick={this.handleClickIncrement} className="full-width">Increment</button>
+        <button onClick={this.handleClickDecrement} className="full-width">Decrement</button>
+        <button onClick={this.handleClickReset} className="full-width">Reset</button>
       </section>
     );
   }
